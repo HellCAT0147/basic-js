15a9ca5237c48f792d7d6f031da507353ffdcacb
HellCAT0147 <letscheckmail23@gmail.com>
2023-04-12 20:44:12 +0500

    feat: add first 2 tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,15 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity == 'string') {
+    let converted = +sampleActivity
+    if (typeof converted == 'number' && !isNaN(converted)) {
+      let result = Math.ceil(Math.log(MODERN_ACTIVITY / converted) / (0.693 / HALF_LIFE_PERIOD));
+      if (result > 0 && isFinite(result)) return result;
+    }
+  }
+  return false;
 }
 
 module.exports = {
--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(backyard) {
+  let cats = [];
+  for (let place of backyard) {
+    cats.push(...place.filter(thing => thing == '^^'));
+  }
+  return cats.length;
 }
 
 module.exports = {
